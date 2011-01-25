3b473e053aceac5cefb0993153a7b528cde07cd7
Nicholas Young <nicholas@nicholaswyoung.com>
2011-01-24 20:15:39 -0600

    First release.

---
+++ js/site.js
@@ -0,0 +1,15 @@
+$(function() {
+	var getVars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for(var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		getVars.push(hash[0]);
+		getVars[hash[0]] = hash[1];
+	}
+	
+	if(getVars.action != '') {
+		if($('section#messages section#' + getVars.action).length > 0) {
+			$('section#messages section#' + getVars.action).slideDown();
+		}
+	}
+});
